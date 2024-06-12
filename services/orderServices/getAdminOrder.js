bbfeab19780729648548a2fedf707e732310925f
belal mohamed <belalagwa0@gmail.com>
2024-06-13 00:20:48 +0200

    get all complete and cancelled order and get pennding to user and store

---
+++ services/orderServices/getAdminOrder.js
@@ -0,0 +1,68 @@
+const asyncHandler = require("express-async-handler");
+const OrderModel = require("../../modules/orderModel");
+const ApiError = require("../../utils/apiError/apiError");
+
+// //  @dec  get all complete order and cancelled Associated with store
+// //  @route  Get /api/v1/orders/user
+// //  @access Protect/user
+exports.getAllCompleteStoreOrder = asyncHandler(async (req, res, next) => {
+  const completedOrCancelledOrders = await OrderModel.find({
+    store: req.body.store,
+    status: { $in: ["Delivered", "Cancelled"] },
+  })
+    .sort({ createdAt: -1 });
+
+  if (!completedOrCancelledOrders) {
+    return next(
+      new ApiError(
+        `There are no orders that have been completed or cancelled yet.`,
+        404
+      )
+    );
+  }
+  res.status(200).send({
+    status: true,
+    message: "success to get complete orders Associated with store",
+    data: completedOrCancelledOrders,
+  });
+});
+
+// //  @dec  get all pending order to store
+// //  @route  Get /api/v1/orders/store/pending
+// //  @access Protect/store
+exports.getAllPendingStoreOrder = asyncHandler(async (req, res, next) => {
+  const storeId = req.body.store;
+  const storeOrders = await OrderModel.aggregate([
+    {
+      $match: {
+        store: storeId,
+        status: { $in: ["Pending", "Store Approved", "Admin Approved"] },
+      },
+    },
+    {
+      $addFields: {
+        statusPriority: {
+          $switch: {
+            branches: [
+              { case: { $eq: ["$status", "Pending"] }, then: 1 },
+              { case: { $eq: ["$status", "Admin Approved"] }, then: 2 },
+              { case: { $eq: ["$status", "Store Approved"] }, then: 3 },
+            ],
+            default: 4,
+          },
+        },
+      },
+    },
+    { $sort: { statusPriority: 1, createdAt: -1 } },
+    { $project: { statusPriority: 0 } },
+  ]);
+
+  if (!storeOrders) {
+    return next(new ApiError(`There are no recent orders.`, 404));
+  }
+  res.status(200).send({
+    status: true,
+    message: "success to get store pending orders",
+    data: storeOrders,
+  });
+});
---
+++ services/orderServices/getUserOrder.js
@@ -0,0 +1,62 @@
+const asyncHandler = require("express-async-handler");
+const OrderModel = require("../../modules/orderModel");
+const ApiError = require("../../utils/apiError/apiError");
+
+// //  @dec  get all complete order and cancelled to user
+// //  @route  Get /api/v1/orders/user
+// //  @access Protect/user
+exports.getAllCompleteUserOrder = asyncHandler(async (req, res, next) => {
+  const completedOrCancelledOrders = await OrderModel.find({
+    user: req.userModel._id,
+    status: { $in: ["Delivered", "Cancelled"] },
+  })
+    .sort({ createdAt: -1 })
+    .limit(25);
+
+  if (!completedOrCancelledOrders) {
+    return next(
+      new ApiError(
+        `There are no orders that have been completed or cancelled yet.`,
+        404
+      )
+    );
+  }
+  res
+    .status(200)
+    .send({
+      status: true,
+      message: "success to get user orders",
+      data: completedOrCancelledOrders,
+    });
+});
+
+
+
+// //  @dec  get all pending order to user
+// //  @route  Get /api/v1/orders/user/pending
+// //  @access Protect/user
+exports.getAllPendingUserOrder = asyncHandler(async (req, res, next) => {
+    const pendingOrders = await OrderModel.find({
+      user: req.userModel._id,
+      status: { $ne: ["Delivered", "Cancelled"] },
+    })
+      .sort({ createdAt: -1 })
+     
+  
+    if (!pendingOrders) {
+      return next(
+        new ApiError(
+          `There are no recent orders.`,
+          404
+        )
+      );
+    }
+    res
+      .status(200)
+      .send({
+        status: true,
+        message: "success to get user pending orders",
+        data: pendingOrders,
+      });
+  });
+  
